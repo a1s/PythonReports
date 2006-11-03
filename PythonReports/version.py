f14f43008f414430a0de5cc1fa2dc42ee10b2a76
Aleksandr Smyshliaev <alex@gorka.lv>
2006-11-03 12:28:19 +0000

    PythonReports package version declaration

---
+++ PythonReports/version.py
@@ -0,0 +1,14 @@
+"""PythonReports package version declaration"""
+
+"""History:
+03-nov-2006 [als]   created
+"""
+
+# Note: __date__ is a string because all modules have __date_ as a string.
+# wouldn't datetime.date object be better?
+
+__version__ = "0.1.0"
+__date__ = "2006-11-03"
+__all__ = ["__version__", "__date__"]
+
+# vim: set et sts=4 sw=4 :

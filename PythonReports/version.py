570ce5516a563e7449b88a436f9c46f436a01595
Aleksandr Smyshliaev <alex@gorka.lv>
2006-11-07 07:23:15 +0000

    version 0.2.0

--- PythonReports/version.py
+++ PythonReports/version.py
@@ -7,8 +7,8 @@
 # Note: __date__ is a string because all modules have __date_ as a string.
 # wouldn't datetime.date object be better?
 
-__version__ = "0.1.0"
-__date__ = "2006-11-03"
+__version__ = "0.2.0"
+__date__ = "2006-11-07"
 __all__ = ["__version__", "__date__"]
 
 # vim: set et sts=4 sw=4 :

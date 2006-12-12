813120281c0bf0a24de7ddca2f73d971e28fbfb4
Aleksandr Smyshliaev <alex@gorka.lv>
2006-12-12 12:44:11 +0000

    .size renamed to .height for compatibility with base class (.chop was broken)

--- PythonReports/RLDrivers.py
+++ PythonReports/RLDrivers.py
@@ -5,11 +5,13 @@ so we can just use PIL ImageDriver instead of wrapping a wrapper.
 
 """
 """History (most recent first):
+12-dec-2006 [als]   .size renamed to .height for compatibility with base class
+                    (.chop was broken)
 01-nov-2006 [als]   driver classes have backend name property
 05-oct-2006 [als]   created
 """
-__version__ = "$Revision: 1.3 $"[11:-2]
-__date__ = "$Date: 2006/12/06 16:55:16 $"[7:-2]
+__version__ = "$Revision: 1.4 $"[11:-2]
+__date__ = "$Date: 2006/12/12 10:44:11 $"[7:-2]
 
 __all__ = ["TextDriver"]
 
@@ -27,11 +29,6 @@ class TextDriver(drivers.TextDriver):
     """
 
     backend = "RL"
-
-    # attributes read directly by PythonReports.PdfWriter
-    name = None
-    size = None
-    leading = None
 
     # 1/5 of character size is default line gap used by ReportLab.
     DEFAULT_LEADING = .2
@@ -62,15 +59,15 @@ class TextDriver(drivers.TextDriver):
                 fonts.fontfile(_typeface, _bold, _italic)))
             self._registered_fonts.add(_name)
         self.name = _name
-        self.size = _size
+        self.height = _size
         self.leading = _size * self.DEFAULT_LEADING
 
     def getsize(self, text):
         """Return size tuple (width, height) for given text"""
         # have to aggregate dimensions manually
         _lines = text.split("\n")
-        _height = (self.size + self.leading) * len(_lines) - self.leading
-        _width = max([pdfmetrics.stringWidth(_line, self.name, self.size)
+        _height = (self.height + self.leading) * len(_lines) - self.leading
+        _width = max([pdfmetrics.stringWidth(_line, self.name, self.height)
             for _line in _lines])
         return (_width, _height)
 

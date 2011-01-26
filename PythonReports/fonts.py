07b64fd3c7b46a2a927cd70cf077d923f83e589c
Alexey Luchko <a-luch@users.sourceforge.net>
2011-01-26 15:53:59 +0000

    try to load windows fonts once when a font is missing

--- PythonReports/fonts.py
+++ PythonReports/fonts.py
@@ -1,5 +1,6 @@
 """Fonts registry"""
 """History (most recent first):
+26-jan-2011 [luch]  try to load windows fonts once when a font is missing
 26-jan-2011 [als]   more well-known font file names;
                     add Mac OS X font paths
 22-dec-2007 [als]   add Ubuntu fonts location to SYSFONTPATHS (sf bug 1856408)
@@ -7,13 +8,15 @@
 26-sep-2006 [als]   add font paths on windows (required by reportlab)
 26-sep-2006 [als]   created
 """
-__version__ = "$Revision: 1.4 $"[11:-2]
-__date__ = "$Date: 2011/01/26 07:43:39 $"[7:-2]
+__version__ = "$Revision: 1.5 $"[11:-2]
+__date__ = "$Date: 2011/01/26 13:53:59 $"[7:-2]
 
 __all__ = ["fontfile", "register"]
 
 from operator import itemgetter
 import os, sys
+
+import windows_fonts
 
 # well-known font files
 FONTS = {
@@ -76,6 +79,8 @@ FONTS = {
     None: "cour.ttf",
 }
 
+WINDOWS_FONTS_ADDED = False
+
 # paths to look for TrueType fonts
 SYSFONTPATHS = []
 if os.name == "nt":
@@ -88,9 +93,9 @@ elif os.name == "posix":
         "/usr/share/fonts/corefonts", # Gentoo Linux (TM)
         "/usr/share/fonts/truetype/msttcorefonts", # Ubuntu (Feisty)
     ])
-if sys.platform == "darwin":
-    SYSFONTPATHS.extend(map(itemgetter(0), os.walk("/Library/Fonts")))
-    FONTS[None] = "Courier New.ttf"
+    if sys.platform == "darwin":
+        SYSFONTPATHS.extend(map(itemgetter(0), os.walk("/Library/Fonts")))
+        FONTS[None] = "Courier New.ttf"
 
 def fontfile(typeface, bold=False, italic=False):
     """Return TTF file name for a font
@@ -101,10 +106,15 @@ def fontfile(typeface, bold=False, italic=False):
         italic: True for italic font
 
     """
+    _font_key = (typeface, bool(bold), bool(italic))
     try:
-        _file = FONTS[(typeface, bool(bold), bool(italic))]
+        _file = FONTS[_font_key]
     except KeyError:
-        _file = FONTS[None]
+        add_windows_fonts()
+        try:
+            _file = FONTS[_font_key]
+        except:
+            _file = FONTS[None]
     if os.path.dirname(_file) == "":
         # file name does not contain directory path.
         # the font must be in the system fonts directory.
@@ -116,12 +126,19 @@ def fontfile(typeface, bold=False, italic=False):
                 # to skip the search next time
                 # WARNING: this will populate the registry
                 # with default font for each unknown typeface
-                FONTS[(typeface, bool(bold), bool(italic))] = _file
+                FONTS[_font_key] = _file
                 break
         else:
             # PIL raises IOError when font file does not exist.  so do we.
             raise IOError("Cannot locate font file %r" % _file)
     return _file
+
+def add_windows_fonts():
+    """Register installed windows fonts"""
+    global WINDOWS_FONTS_ADDED
+    if not WINDOWS_FONTS_ADDED:
+        FONTS.update(windows_fonts.ls_ttf())
+        WINDOWS_FONTS_ADDED = True
 
 def register(filename, typeface, bold=False, italic=False):
     """Register non-standard TTF file

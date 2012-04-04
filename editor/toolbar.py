72d9f83e9c0da24f0e2e8b0b84c82dcfb5a05dce
KACAH <kacah222@gmail.com>
2012-04-04 16:03:22 +0300

    Added new icons on toolbar and window

--- editor/toolbar.py
+++ editor/toolbar.py
@@ -16,9 +16,11 @@ class FileToolbar(wxaui.AuiToolBar):
             agwStyle=wxaui.AUI_TB_DEFAULT_STYLE)
         self.SetToolBitmapSize(wx.Size(24, 24))
 
-        self.AddSimpleTool(1, "New template", utils.get_icon("new.png"))
-        self.AddSimpleTool(2, "Open template", utils.get_icon("open.png"))
-        self.AddSimpleTool(3, "Save template", utils.get_icon("save.png"))
+        self.AddSimpleTool(1, "New", utils.get_icon("new.png"), "New template")
+        self.AddSimpleTool(2, "Open", utils.get_icon("open.png"),
+            "Open template")
+        self.AddSimpleTool(3, "Save", utils.get_icon("save.png"),
+            "Save template")
 
         self.Realize()
 
@@ -30,10 +32,18 @@ class VisualToolbar(wxaui.AuiToolBar):
             agwStyle=wxaui.AUI_TB_DEFAULT_STYLE)
         self.SetToolBitmapSize(wx.Size(24, 24))
 
-        self.AddSimpleTool(1, "Create field", utils.get_icon("field.png"))
-        self.AddSimpleTool(2, "Create line", utils.get_icon("line.png"))
-        self.AddSimpleTool(3, "Create rectangle", utils.get_icon("rect.png"))
-        self.AddSimpleTool(4, "Create image", utils.get_icon("image.png"))
-        self.AddSimpleTool(5, "Create barcode", utils.get_icon("barcode.png"))
+        self.AddSimpleTool(1, "Arrow", utils.get_icon("arrow.png"),
+            "Select elements")
+        self.AddSeparator()
+        self.AddSimpleTool(2, "Field", utils.get_icon("field.png"),
+            "Create field")
+        self.AddSimpleTool(3, "Line", utils.get_icon("line.png"),
+            "Create line")
+        self.AddSimpleTool(4, "Rectangle", utils.get_icon("rect.png"),
+            "Create rectangle")
+        self.AddSimpleTool(5, "Image", utils.get_icon("image.png"),
+            "Create image")
+        self.AddSimpleTool(6, "Barcode", utils.get_icon("barcode.png"),
+            "Create barcode")
 
         self.Realize()

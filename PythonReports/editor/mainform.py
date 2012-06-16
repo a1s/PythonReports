a3def8e065c4133b419e58d6830171932416a2a3
alexander smishlajev <alex@gorka.lv>
2012-06-16 15:58:56 +0300

    Switch to Glyphicons

--- PythonReports/editor/mainform.py
+++ PythonReports/editor/mainform.py
@@ -1,8 +1,9 @@
 """Main frame of editor"""
 """
-20-mar-2012 [kacah]   created
-
+16-jun-2012 [als]   New pane icons based on Glyphicons
+20-mar-2012 [kacah] created
 """
+
 import wx
 import wx.lib.agw.aui as wxaui
 import wx.lib.ogl as wxogl
@@ -15,8 +16,8 @@ from toolbar import FileToolbar, VisualToolbar
 import utils
 from workspace import Workspace
 
-FORM_TITLE = "PythonReports editor"
-INTRO_TEXT = "Welcome to PythonReports editor"
+FORM_TITLE = "PythonReports Template Editor"
+INTRO_TEXT = "Welcome to PythonReports Template Editor"
 
 class EditorForm(wx.Frame):
     """Main frame of editor"""
@@ -63,7 +64,7 @@ class EditorForm(wx.Frame):
         self.aui_mgr.AddPane(self.shell, wxaui.AuiPaneInfo()
             .Name("Shell").Caption("Shell").Bottom()
             .CloseButton(False).MinimizeButton(True)
-            .Icon(utils.get_icon("shell.png")))
+            .Icon(utils.get_icon("137_computer_service.png")))
 
         self.aui_mgr.AddPane(self.property_grid, wxaui.AuiPaneInfo()
             .Name("PropGrid").Caption("Properties").Right()
@@ -73,7 +74,7 @@ class EditorForm(wx.Frame):
         self.aui_mgr.AddPane(self.elements_tree, wxaui.AuiPaneInfo()
             .Name("ElemTree").Caption("Elements").Left()
             .CloseButton(False).MinimizeButton(True)
-            .Icon(utils.get_icon("tree.png")))
+            .Icon(utils.get_icon("114_list.png")))
 
         self.aui_mgr.AddPane(self.workspace, wx.CENTER)
         self.aui_mgr.AddPane(self.file_toolbar, wxaui.AuiPaneInfo().Name
@@ -97,3 +98,5 @@ class EditorForm(wx.Frame):
             self.aui_mgr.UnInit()
             wxogl.OGLCleanUp()
             self.Destroy()
+
+# vim: set et sts=4 sw=4 :

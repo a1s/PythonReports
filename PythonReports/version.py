e2cf36f3cad3ed8517570de131ef0eb5cc94c847
alexander smishlajev <alex@gorka.lv>
2012-12-08 17:11:47 +0200

    Version 0.6.1.

--- PythonReports/version.py
+++ PythonReports/version.py
@@ -2,8 +2,8 @@
 
 from datetime import date
 
-__version__ = "0.6.0"
-__date__ = date(2012, 11, 16)
+__version__ = "0.6.1"
+__date__ = date(2012, 12, 8)
 __all__ = ["__version__", "__date__"]
 
 # vim: set et sts=4 sw=4 :
